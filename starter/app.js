6b3a6fc7a17448497de0ba8552228fe6328845a0
Vagelis Giannakosian <e.giannakosian@gmail.com>
2019-04-21 15:06:09 +0300

    implemented the data structure of the project and added create newItem function to the data controller as well as exposed it publicly for the global controller

--- starter/app.js
+++ starter/app.js
@@ -1,14 +1,66 @@
-//Data Controller module
+/**
+ * Data Controller module
+ */
+
 var budgetController = (function(){
 
+ var Expense = function(id,description,value){
+     this.id = id;
+     this.description = description;
+     this.value = value;
+ };
 
-    //some code
+ var Income = function(id,description,value){
+     this.id = id;
+     this.description = description;
+     this.value = value;
+ };
 
+ var data = {
+     allItems:{
+         exp : [],
+         inc : [],
 
+     },
+    totals:{
+        exp : 0,
+        inc: 0,
+    }
+ };
+
+ return{
+     addItem: function(type,des,val){
+         var newItem, ID;
+         //create new ID
+         if(data.allItems[type].length > 0){
+             ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+         }else{
+             ID = 0;
+         }
+
+         //create new item based on type
+         if(type==='exp'){
+             newItem = new Expense(ID,des,val);
+         }else if(type==='inc'){
+             newItem = new Income(ID,des,val);
+         }
+         //push it into data structure
+         data.allItems[type].push(newItem);
+         //return new item
+         return newItem;
+     },
+     testing: function(){
+         console.log(data);
+     }
+
+ };
 
 })();
 
-//Ui Controller module
+
+/**
+ * Ui Controller module
+ */
 var UIController = (function(){
       var DOMstring = {
          inputType: '.add__type',
@@ -34,34 +86,51 @@ var UIController = (function(){
 
 
    };
+
 })();
 
 
 
-//Global App Controlller module
+/**
+ * Global App Controlller module
+ */
 var controller = (function(budgetCtrl,UICtrl){
-   var DOM = UICtrl.getDOMstrings();
+
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click',ctrlAddItem);
+
+        document.addEventListener('keypress',function(e){
+            if(e.key === 'Enter'){
+                ctrlAddItem();
+            }
+        });
+
+
+    };
+
 
    var ctrlAddItem = function(){
-      //get the input data
-      var input = UICtrl.getInput();
-console.log(input);
-      //TODO: add the item to the budget controller
+       var input, newItem;
+      //1. get the input data
+       input = UICtrl.getInput();
+
+      //2. add the item to the budget controller
+       newItem = budgetCtrl.addItem(input.type,input.description,input.value);
       //TODO: add the new item to the user interface
       //TODO: calculate the budget
       //TODO: display the budget on the UI
-   }
+   };
+
+    return{
+       init: function(){
+           setupEventListeners();
+       }
 
 
-   document.querySelector(DOM.inputButton).addEventListener('click',ctrlAddItem);
-
-
-   document.addEventListener('keypress',function(e){
-      if(e.key === 'Enter'){
-         ctrlAddItem();
-      }
-   });
-
-
+    };
 
 })(budgetController,UIController);
+
+controller.init();
